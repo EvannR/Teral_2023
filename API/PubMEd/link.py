dc6d5972a57abb51c644ef1dea0a93d7791d7c66
EvannR <evannrabeau@gmail.com>
2023-11-23 15:14:32 -0500

    23/11

---
+++ API/PubMEd/link.py
@@ -0,0 +1,9 @@
+def generate_pubmed_link(pmid):
+    base_url = "https://pubmed.ncbi.nlm.nih.gov/"
+    pubmed_link = f"{base_url}{pmid}/"
+    return pubmed_link
+
+# Exemple d'utilisation avec un PMID fictif (remplacez-le par votre propre PMID)
+pmid = "7947353"
+link = generate_pubmed_link(pmid)
+print("Lien PubMed:", link)
